5bc327f76aaa9e12a8fc4d2b9b486eaf88b45804
brewcrew059 <mjbrewer059@gmail.com>
2022-04-25 21:41:39 -0400

    Plz commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,8 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xea994979E010aeA3db1643809E8bA2D88779dd31";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
+
+// Crypto Devs Contract Address: 0xDE8cE2Ad783bd5c6e91C76d16DaD520998deBcfF
